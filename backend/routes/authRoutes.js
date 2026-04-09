b3e210b5a9012d14b8b2c8cf14fac44929025697
akshayrajeshuni-ui <akshayrajeshuni@gmail.com>
2026-04-09 15:15:14 +0530

    add auth routes

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    const user = new User({ email, password: hashed });
+    await user.save();
+
+    res.json({ message: "User registered" });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json("User not found");
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json("Wrong password");
+
+    const token = jwt.sign({ id: user._id }, "secretkey");
+
+    res.json({ token });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const expenseRoutes = require("./routes/expenseRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 
 // ✅ Routes
 app.use("/api/expenses", expenseRoutes);
+app.use("/api/auth", authRoutes);
 
 // ✅ MongoDB
 mongoose.connect(process.env.MONGO_URI)
